ed5c36fc34cf58f540d963969133819f62807740
Adam Gerecke <adamgerecke@quickenloans.com>
2020-11-02 16:41:28 -0500

    Added Countdown

--- Dev.Build_Final/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Dev.Build_Final/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -8,6 +8,20 @@ import { Component } from '@angular/core';
 export class NavMenuComponent {
   isExpanded = false;
 
+
+  xmasDay = new Date("december 25, 2020 00:00:00").getTime();
+
+  countDown: any;
+  x = setInterval(() => {
+    var now = new Date().getTime();
+    var distance = this.xmasDay - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    this.countDown = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  })
+
   collapse() {
     this.isExpanded = false;
   }
